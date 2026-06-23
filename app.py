c0c6e3ff579c5c2eb8f939ab638c58485bc3c000
Naufal <surya_harnaufal0@students.unnes.ac.id>
2026-06-23 15:26:01 +0700

    Refactor face detection and dataset loading functions

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ with tab2:
             st.subheader("Akurasi Menggunakan Metode Euclidean")
             st.metric(label="Nilai Akurasi", value=f"{acc_e:.2f} %")
             if acc_e >= 50.0: st.success("Sukses: Di atas target 50%!")
-            else: st.error("❌ Atur Slider Komponen Utama ke angka 3 atau 4.")
+            else: st.error("Atur Slider Komponen Utama ke angka 3 atau 4.")
         with c_acc2:
             st.subheader("Akurasi Menggunakan Metode Cosine Similarity")
             st.metric(label="Nilai Akurasi", value=f"{acc_c:.2f} %")
