be4d23abfc07dc630e1c1f2ea48723a2b8217682
Naufal <surya_harnaufal0@students.unnes.ac.id>
2026-06-23 19:13:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -173,8 +173,12 @@ with tab3:
             
             img_a_display = cv2.cvtColor(cv2.imread(img_path_a), cv2.COLOR_BGR2RGB)
             img_b_display = cv2.cvtColor(cv2.imread(img_path_b), cv2.COLOR_BGR2RGB)
-            st.image([img_a_display], caption=["Foto Masa Kecil"], width=240)
-            st.image([img_b_display], caption=["Foto Masa Dewasa"], width=240)
+
+            foto_col1, foto_col2 = st.columns(2)
+            with foto_col1:
+                st.image([img_a_display], caption=["Foto Masa Kecil"], width=240)
+            with foto_col2:
+                st.image([img_b_display], caption=["Foto Masa Dewasa"], width=240)
             
             res_col1, res_col2 = st.columns(2)
             with res_col1:
